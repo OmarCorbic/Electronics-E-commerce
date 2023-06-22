e6528fb265bf17cef6ec6e49961cf3b69c897650
Omar Corbic <omar_corbic@hotmail.com>
2023-06-22 15:37:15 +0200

    Simplified the modal component

--- client-side/src/components/Modal.jsx
+++ client-side/src/components/Modal.jsx
@@ -1,15 +1,12 @@
-import React from "react";
-
-const Modal = ({ hideModal, render }) => {
+const Modal = ({ hideModal, render, children }) => {
   return (
-    <div onClick={hideModal} className="modal-background">
-      <div className="modal-container">
-        <div className="modal-title"></div>
-        <div className="modal-body">{render()}</div>
-        <div className="modal-footer">
-          <button onClick={hideModal}>Cancel</button>
-          <button>Continue</button>
-        </div>
+    <div
+      onClick={hideModal}
+      id="background"
+      className="fixed flex items-center justify-center top-0 left-0 w-full h-full bg-black bg-opacity-60 z-[9997]"
+    >
+      <div className="h-[75%] w-[33.333%] rounded-md p-5 bg-white bg-opacity-100 flex">
+        <div className="flex-1">{children}</div>
       </div>
     </div>
   );
