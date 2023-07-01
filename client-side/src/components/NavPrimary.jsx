6e726f3c93698c7cc2fc202cfa10a5e8f89f909e
Omar Corbic <omar_corbic@hotmail.com>
2023-07-01 14:45:27 +0200

    Added Sign up, Log in and Profile buttons to nav

--- client-side/src/components/NavPrimary.jsx
+++ client-side/src/components/NavPrimary.jsx
@@ -1,10 +1,22 @@
-import { AiOutlineShoppingCart, AiOutlineHeart } from "react-icons/ai";
+import {
+  AiOutlineShoppingCart,
+  AiOutlineHeart,
+  AiOutlineLogin,
+  AiOutlineUserAdd,
+  AiOutlineUser,
+} from "react-icons/ai";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import SearchBar from "./SearchBar";
 
-const PrimaryNav = ({ handleCartClick, handleWishlistClick }) => {
+const PrimaryNav = ({
+  handleCartClick,
+  handleWishlistClick,
+  handleLogInClick,
+  handleSignUpClick,
+}) => {
   const { quantity } = useSelector((state) => state.cart);
+  const loggedIn = true;
 
   return (
     <nav className="primary-navbar contain">
@@ -17,12 +29,39 @@ const PrimaryNav = ({ handleCartClick, handleWishlistClick }) => {
       <SearchBar />
 
       <div className="right-side">
-        <div onClick={handleWishlistClick} className="wishlist-btn btn">
-          <div className="icon">
-            <AiOutlineHeart size="26" />
-          </div>
-          <span>Your Wishlist</span>
-        </div>
+        {loggedIn && (
+          <>
+            <div onClick={handleWishlistClick} className="wishlist-btn btn">
+              <div className="icon">
+                <AiOutlineHeart size="26" />
+              </div>
+              <span>Wishlist</span>
+            </div>
+            <Link to="/profile" className="wishlist-btn btn">
+              <div className="icon">
+                <AiOutlineUser size="26" />
+              </div>
+              <span>Profile</span>
+            </Link>
+          </>
+        )}
+
+        {!loggedIn && (
+          <>
+            <div onClick={handleLogInClick} className="wishlist-btn btn">
+              <div className="icon">
+                <AiOutlineLogin size="26" />
+              </div>
+              <span>Log in</span>
+            </div>
+            <div onClick={handleSignUpClick} className="wishlist-btn btn">
+              <div className="icon">
+                <AiOutlineUserAdd size="26" />
+              </div>
+              <span>Sign up</span>
+            </div>
+          </>
+        )}
 
         <div onClick={handleCartClick} className="cart-btn btn">
           <div className="icon">
@@ -31,7 +70,7 @@ const PrimaryNav = ({ handleCartClick, handleWishlistClick }) => {
             ) : null}
             <AiOutlineShoppingCart size="26" />
           </div>
-          <span>Your Cart</span>
+          <span>Cart</span>
         </div>
       </div>
     </nav>
