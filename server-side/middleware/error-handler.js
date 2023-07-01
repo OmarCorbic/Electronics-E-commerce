da6cee228961921cc5a0e15026f590a00e29d8d9
Omar Corbic <omar_corbic@hotmail.com>
2023-07-01 14:50:25 +0200

    Added custom error handling

--- server-side/middleware/error-handler.js
+++ server-side/middleware/error-handler.js
@@ -1,9 +1,30 @@
+const { StatusCodes } = require("http-status-codes");
+
 const errorHandlerMiddleware = (err, req, res, next) => {
-  console.log(err);
-  if (err.message) {
-    return res.status(500).json({ msg: err.message });
+  const customError = {
+    message: err.message || "Something went wrong. Please try again later",
+    statusCode: err.statusCode || 500,
+  };
+
+  if (err.name && err.name === "ValidationError") {
+    customError.message = Object.values(err.errors)
+      .map((err) => err.message)
+      .join(". ");
+    customError.statusCode = StatusCodes.BAD_REQUEST;
   }
-  res.status(500).json({ msg: "Something went wrong. Please try again later" });
+
+  if (err.name && err.name === "CastError") {
+    customError.message = `Invalid ${err.path} field`;
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+
+  if (err.code && err.code === 11000) {
+    customError.msg = `Duplicate value entered for ${Object.keys(
+      err.keyValue
+    )} field, please choose another one.`;
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+  res.status(customError.statusCode).json({ message: customError.message });
 };
 
 module.exports = errorHandlerMiddleware;
