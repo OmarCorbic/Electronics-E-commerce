1b2a3da050b424fafda80565cbf981263d649b69
Omar Corbic <omar_corbic@hotmail.com>
2023-07-01 14:52:09 +0200

    Added modal reducer

--- client-side/src/store/store.js
+++ client-side/src/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "../features/cart/cartSlice";
 import progressReducer from "../features/progress/progressSlice";
+import modalReducer from "../features/modal/modalSlice";
 
 const store = configureStore({
   reducer: {
     cart: cartReducer,
     progress: progressReducer,
+    modal: modalReducer,
   },
 });
 
