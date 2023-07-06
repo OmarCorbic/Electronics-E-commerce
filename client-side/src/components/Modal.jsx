c71ab01194695f1e2b0ad64ecca047abedb18031
Omar Corbic <omar_corbic@hotmail.com>
2023-07-06 17:01:24 +0200

    Changed modal height from 75% to 80%

--- client-side/src/components/Modal.jsx
+++ client-side/src/components/Modal.jsx
@@ -5,7 +5,7 @@ const Modal = ({ hideModal, render, children }) => {
       id="background"
       className="fixed flex items-center justify-center top-0 left-0 w-full h-full bg-black bg-opacity-60 z-[9997]"
     >
-      <div className="h-[75%] w-[33.333%] rounded-md p-5 bg-white bg-opacity-100 flex">
+      <div className="h-[80%] w-[33.333%] rounded-md p-5 bg-white bg-opacity-100 flex">
         <div className="flex-1">{children}</div>
       </div>
     </div>
