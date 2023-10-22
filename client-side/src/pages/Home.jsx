fbfe248022a5a1aa9329d62f82fb3287aaf97e38
Omar Corbic <omar_corbic@hotmail.com>
2023-10-22 12:52:53 +0200

    Added hero section to home page

--- client-side/src/pages/Home.jsx
+++ client-side/src/pages/Home.jsx
@@ -1,36 +1,38 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import Spinner from "../components/Spinner";
+import React, { useRef } from "react";
+import heroImg from "../images/hero-image.jpg";
+import { Link } from "react-router-dom";
 
 const Home = () => {
+  const imgRef = useRef();
+  const blurImg = () => {
+    imgRef.current.classList.add("blur-sm");
+  };
+  const unblurImg = () => {
+    imgRef.current.classList.remove("blur-sm");
+  };
   return (
-    <div className="flex items-center justify-center px-[8%]">
-      {/* <section className="flex flex-col items-center justify-between lg:flex-row">
-        <NavLink to="/products/laptops">
-          <article className="flex h-60 w-72 cursor-pointer items-center justify-center bg-['../../images/laptops.png'] bg-contain bg-center bg-no-repeat shadow-xl transition duration-500">
-            <div className="overlay">
-              <div>Laptops Collection</div>
-              <div>Shop now</div>
-            </div>
-          </article>
-        </NavLink>
-        <NavLink to="/products/accessories">
-          <article className="flex h-60 w-72 cursor-pointer items-center justify-center shadow-xl transition duration-500">
-            <div className="overlay">
-              <div>Accessoriess Collection</div>
-              <div>Shop now</div>
-            </div>
-          </article>
-        </NavLink>
-        <NavLink to="/products/cameras">
-          <article className="flex h-60 w-72 cursor-pointer items-center justify-center shadow-xl transition duration-500">
-            <div className="overlay">
-              <div>Cameras Collection</div>
-              <div>Shop now</div>
-            </div>
-          </article>
-        </NavLink>
-      </section> */}
+    <div className="flex flex-col items-center justify-center">
+      <section className="relative flex w-full items-center justify-center lg:justify-start">
+        <div
+          ref={imgRef}
+          style={{ backgroundImage: `url(${heroImg})` }}
+          className="h-72 w-full bg-cover bg-center bg-no-repeat brightness-50 sm:h-[90vh]   lg:brightness-75 "
+        ></div>
+        <div className="absolute p-3 text-center text-base font-bold text-white lg:flex lg:h-full lg:w-1/3 lg:items-center lg:gap-3 lg:bg-opacity-50 lg:bg-gradient-to-r lg:from-[rgba(0,0,0,0.9)]  lg:via-[rgba(0,0,0,0.7)] lg:to-transparent lg:p-5 lg:text-2xl">
+          <p>Set your self up with the coolest gear!</p>
+          <Link className="md:w-52" to="/products/accessories">
+            <button
+              onMouseEnter={blurImg}
+              onMouseLeave={unblurImg}
+              className=" mt-2 border-2 border-white bg-orange-600 p-3 text-sm  md:text-lg"
+            >
+              Shop now!
+            </button>
+          </Link>
+        </div>
+      </section>
+
+      <section></section>
     </div>
   );
 };
